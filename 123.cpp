d26be97e09d681f6811c63ac0e8e475ce3c5d4b3
Bozhok <38132479+Prough123@users.noreply.github.com>
2018-04-19 13:04:26 +0300

    Пароль

--- 123.cpp
+++ 123.cpp
@@ -1,63 +1,49 @@
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include <conio.h>
-#include <windows.h>
-using namespace std;
-int main()
-{
-	int b,player=0,comp=0;
-	srand(time(NULL));
-	cout<<"press enter to begin game..."<<endl;
-	getch();
-	b=rand()%2+1;
-	switch(b)
-	{
-	case 1:
-		{
-			for(int i=0;i<4;i++)
-			{
-				cout<<"player first to do cast"<<endl;
-				cout<<"press enter..."<<endl;
-				getch();
-				b=rand()%6+1;
-				player+=b;
-				cout<<"throw from player = "<<b<<endl;
-				cout<<"copucter to do a throw"<<endl;
-				Sleep(100);
-				b=rand()%6+1;
-				comp+=b;
-				cout<<"throw from compucter = "<<b<<endl;
-			}
-			break;
-		}
-	case 2:
-		{
-			for(int i=0;i<4;i++)
-			{
-				cout<<"copucter first to do a throw"<<endl;
-				Sleep(1);
-				b=rand()%6+1;
-				comp+=b;
-				cout<<"throw from compucter = "<<b<<endl;
-				cout<<"player to do cast"<<endl;
-				cout<<"press enter..."<<endl;
-				getch();
-				b=rand()%6+1;
-				player+=b;
-				cout<<"throw from player = "<<b<<endl;
-			}
-			break;
-		}
-	}
-			if(player>comp)
-		{
-			cout<<"player win, with score= "<<player<<endl;
-		}
-			else
-			{
-				cout<<"computer win, with score= "<<comp<<endl;
-			}
-	getch();
-	return 0;
+#include <iostream> 
+#include <time.h> 
+#include <conio.h> 
+#include <stdlib.h> 
+ 
+using namespace std; 
+ 
+void main() 
+{ 
+ setlocale (LC_ALL, "Russian"); 
+ srand(time(NULL)); 
+ int N; 
+ int g = 0; 
+ cout « "Добро пожаловать в генератор пароля. Если у вас нету фантазии мы вам поможем!"«endl; 
+ cout « "Если вы хотите, чтобы в вашем пароле были только цифры введите 0!\n"; 
+ cout « "Если вы хотите, чтобы в вашем пароле были только буквы введите 1!\n"; 
+ cout « "Если вы хотите, чтобы в вашем пароле были и цифры и буквы введите 2!\n"; 
+ cin » g; 
+ cout « "Введите длину пароля:\n"; 
+ cin» N; 
+ char *pass = new char[N + 1]; 
+ for (int i = 0; i<N; ++ i) 
+  switch (g) 
+ { 
+  case 0: 
+   pass[i] = rand() % 10 + '0'; 
+   break; 
+  case 1: 
+   pass[i] = rand() % 26 + 'A'; 
+  break; 
+  case 2: 
+ { 
+  switch(rand()% 3) 
+  { 
+  case 0: 
+   pass[i] = rand() % 10 + '0'; 
+   break; 
+  case 1: 
+   pass[i] = rand() % 26 + 'A'; 
+   break; 
+  case 2: 
+   pass[i] = rand() % 26 + 'a'; 
+  } 
+ } 
+ } 
+ pass[N] = 0; 
+ cout « pass« endl; 
+ system ("pause"); 
 }
